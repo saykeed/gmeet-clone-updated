4b7e779323d0e010531419c28f7ce97b1fa0e38c
saykeed <saykeed@gmail.com>
2022-03-21 21:00:39 +0100

    tried listening for the caller tracks aswell from the joiner room

--- src/views/JoiningRoom.vue
+++ src/views/JoiningRoom.vue
@@ -93,6 +93,18 @@ export default {
             }
             await localListener()
 
+
+            // listening for tracks from the caller
+            peerConnection.addEventListener('track', event => {
+                console.log('Got remote track:', event.streams[0]);
+                event.streams[0].getTracks().forEach(track => {
+                    console.log('Add a track to the remoteStream:', track);
+                    remoteStream.addTrack(track);
+                    this.$refs.remotevid.srcObject = remoteStream
+                });
+            });
+
+
             // invoking the function that get the offer from the room id the joiner inputed
             getOffer()
         }
@@ -129,7 +141,7 @@ export default {
                         let data = item.doc.data();
                         //console.log(`Got the caller ICE candidate: ${JSON.stringify(data)}`);
                         await peerConnection.addIceCandidate(new RTCIceCandidate(data));
-                        console.log(peerConnection)
+                        //console.log(peerConnection)
                     }
                 })
             })
